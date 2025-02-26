2832b41a555a449bb83a596a51a648d17902f3e6
Nitinkumar432 <nitinraj7488204975@gmail.com>
2025-02-27 04:55:46 +0530

    ReMend :  design home page and register page

--- backend/Seeker/Routes/seeker.js
+++ backend/Seeker/Routes/seeker.js
@@ -6,6 +6,9 @@ const router = express.Router();
 router.get('/',(req,res)=>{
     res.send("hello ji");;
 })
+router.get('/problempage',(req,res)=>{
+    res.render('./Seeker/commonproblem.ejs',)
+})
 router.post("/register", registerSeeker);
 
 export default router;
